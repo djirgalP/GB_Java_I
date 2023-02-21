12fbb78a4bf58fd381f2c56b4a8a4d2d74f12ec3
Djirgal <djirgal.poshtarova@yandex.ru>
2023-02-21 08:14:44 +0300

    2nd task of the Homework1 is done

--- src/Homework1/Homework1_Part2.java
+++ src/Homework1/Homework1_Part2.java
@@ -8,38 +8,31 @@ public class Homework1_Part2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number -> ");
         int N = scanner.nextInt();
-
         for (int i = 1; i <= N; i++) {
             if (isSimpleNumberCheck(i)) {
                 System.out.print(i + " ");
             }
         }
-
-
     }
 
     public static boolean isSimpleNumberCheck(int number) {
-        boolean isSimple = false;
-        int counter = 2;
-        if ((number == 1) || (number == 2) || (number == 3)){
-            System.out.println("1st if, isSimple = " + isSimple);
-
+        if ((number <= 3))
             return true;
-        }
-        for (int i = 2; i < number; i++) {
+
+        int sqrtN = (int) Math.round(Math.sqrt(number));
+        int counter = 1;
+        for (int i = 2; i <= sqrtN; i++) {
             if ((number % i) == 0) {
                 counter++;
             }
-            if (counter > 2) {
-
-                System.out.println("2nd if, isSimple = " + isSimple);
-                return false;
+            if (counter == 2) {
+                break;
             }
         }
-        if (counter == 2){
-            return true;
-        } else {
+        if (counter >= 2){
             return false;
+        } else {
+            return true;
         }
     }
 }
