1fee91b6bc6ef3fbfbd72a326122d1ff4400cd04
Djirgal <djirgal.poshtarova@yandex.ru>
2023-03-09 02:10:30 +0300

    2nd initial commit

--- src/Homework6/Main.java
+++ src/Homework6/Main.java
@@ -1,4 +1,12 @@
 package Homework6;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+
+    }
+    public List<Integer> preorderTraversal(TreeNode root) {
+        return null;
+    }
 }
